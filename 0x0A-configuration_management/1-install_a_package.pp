ee26d09e9dff859823efbd2b5aaae931af66dd2e
Jay-Kip <hanningtonkiptoo14@gmail.com>
2023-11-29 18:25:51 +0300

    Install flask using puppet

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,7 @@
 # install flask 2.1.0
 
-package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
+exec { 'install_flask':
+  command => '/usr/bin/pip3 install Flask==2.1.0 Werkzeug==2.0.2',
+  path    => '/usr/bin',
+  creates => '/usr/local/lib/python3.11/dist-packages/flask',
 }
