bff7f70d4915963e4648b2df585e8de79aa00110
Jay-Kip <hanningtonkiptoo14@gmail.com>
2023-11-30 05:32:16 +0300

    Manifest that configures ubuntu server

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,29 +1,27 @@
-# Script to install nginx using puppet
-
-package {'nginx':
+package { 'nginx':
   ensure => 'present',
 }
 
 # Install nginx
-exec {'install':
+exec { 'install':
   command  => 'sudo apt-get update ; sudo apt-get -y install nginx',
   provider => shell,
 }
 
-# Write  index.html
-exec {'Hello World!':
-  command  => 'echo "Hello World!" | sudo dd status=none of=/var/www/html/index.html',
+# Write index.html
+exec { 'Hello World!':
+  command  => 'echo "Hello World!" | sudo tee /var/www/html/index.html',
   provider => shell,
 }
 
 # Redirect
-exec {'sudo sed -i "s/listen 80 default_server;/listen 80 default_server;\\n\\tlocation \/redirect_me {\\n\\t\\tr
-  eturn 301 https:\/\/www.youtube.com\/;\\n\\t}/" /etc/nginx/sites-available/default':
+exec { 'Add redirect configuration':
+  command  => 'sudo sed -i "s/listen 80 default_server;/listen 80 default_server;\\n\\tlocation \\/redirect_me {\\n\\t\\treturn 301 https:\\/\\/www.youtube.com\\/;\\n\\t}/" /etc/nginx/sites-available/default',
   provider => shell,
 }
 
 # Restart the server
-exec {'run':
+exec { 'Restart NGINX':
   command  => 'sudo service nginx restart',
   provider => shell,
 }
