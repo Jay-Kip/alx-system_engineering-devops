7d71e5dbbefc430c8185e03f1f7d63ce535c0946
Jay-Kip <hanningtonkiptoo14@gmail.com>
2023-11-30 04:09:59 +0300

    Manifest that configures ubuntu server

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -4,21 +4,25 @@ package {'nginx':
   ensure => 'present',
 }
 
+# Install nginx
 exec {'install':
   command  => 'sudo apt-get update ; sudo apt-get -y install nginx',
   provider => shell,
 }
 
+# Write  index.html
 exec {'Hello World!':
   command  => 'echo "Hello World!" | sudo dd status=none of=/var/www/html/index.html',
   provider => shell,
 }
 
+# Redirect
 exec {'sudo sed -i "s/listen 80 default_server;/listen 80 default_server;\\n\\tlocation \/redirect_me {\\n\\t\\tr
   eturn 301 https:\/\/www.youtube.com\/;\\n\\t}/" /etc/nginx/sites-available/default':
   provider => shell,
 }
 
+# Restart the server
 exec {'run':
   command  => 'sudo service nginx restart',
   provider => shell,
