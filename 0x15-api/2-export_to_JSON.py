ca4d22628362f2bbbb4e9b9c0771c938d64b56b9
Jay-Kip <hanningtonkiptoo14@gmail.com>
2024-06-11 01:48:45 +0300

    Export to JSON

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+extend your Python script to export data in the JSON format.
+"""
+
+import csv
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    url = ""
+
+    employee_id = sys.argv[1]
+    user_response = requests.get(url + "users/{}".format(employee_id))
+
+    user = user_response.json()
+    params = {"UserId": employee_id}
+
+    todos_response = requests.get(url + "todos", params=params)
+
+    todos = todos_response.json()
+    tasks - []
+
+    for todo in todos:
+        tasks.append({
+            "tasks": todo.get("title"),
+            "completed": todo.get("completed"),
+            "username": user.get("username")
+        })
+
+    with open(f"{employee_id}.json", "w") as jsonfile:
+        json.dump({employee_id: tasks}, jsonfile, indent=4)
